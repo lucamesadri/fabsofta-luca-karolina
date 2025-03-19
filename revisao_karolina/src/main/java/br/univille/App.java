68a7eeb712f35c19b49498b717f0971cddfc2e24
korrones <karolinaferreira518@gmail.com>
2025-03-18 21:52:05 -0300

    Atualização aula 18/03

--- revisao_karolina/src/main/java/br/univille/App.java
+++ revisao_karolina/src/main/java/br/univille/App.java
@@ -1,9 +1,17 @@
 package br.univille;
+import br.univille.entity.Cidade;
+import br.univille.entity.Pessoa;
+
 public class App { 
     public static void main(String[] args) {
+
+        Cidade joinville = new Cidade();
+        joinville.setNome("Joinville");
+        joinville.setEstado("Santa Catarina");
+
         Pessoa zezinho = new Pessoa("zezinho"); /* Cria uma instancia do objeto pessoa (materialização da classe, uma referencia)) */
         zezinho.setNome("Zezinho"); /*Passando o nome para a variavel */
-
+        zezinho.setCidade(joinville);
         System.out.println(zezinho);
     }
 }
--- revisao_karolina/src/main/java/br/univille/entity/Pessoa.java
+++ revisao_karolina/src/main/java/br/univille/entity/Pessoa.java
@@ -1,9 +1,40 @@
-package br.univille;
+package br.univille.entity;
+
+
 public class Pessoa {
                     /*informação*/
     private String nome; //*Variavel, atributo, propriedade */
+    private Long id;
+    private String endereco;
+    private Cidade cidade;
 
-    /*/Construtor */ /*/Tipo especial dentro do codigo, para obrigar uma pessoa a inserir uma informação */
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /*/Construtor */
+     /*/Tipo especial dentro do codigo, para obrigar uma pessoa a inserir uma informação */
     public Pessoa(String nome) { /*/CTOR ESECREVE ISSO AQUI AUTOAMTICO <- */
         this.nome = nome; /*/ sempre se referencia a classe */
     }
@@ -23,6 +54,7 @@ public class Pessoa {
     // public String toString(){
     //     return getNome();
     // }
+
 }
 
 
