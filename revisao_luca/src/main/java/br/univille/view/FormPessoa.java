0e0f66bde9e79105c4d4bfd2bfc315e2208d87c6
LUCA VINÍCIUS MESADRI <luca.mesadri@univille.br>
2025-03-26 20:31:48 -0300

    feat: controller

--- revisao_luca/src/main/java/br/univille/view/FormPessoa.java
+++ revisao_luca/src/main/java/br/univille/view/FormPessoa.java
@@ -1,19 +1,72 @@
 package br.univille.view;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import br.univille.controller.ControllerPessoa;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 public class FormPessoa extends JFrame {
+    
+    private JLabel lblID, lblNome, lblEndereco;
+    private JTextField txtId, txtNome, txtEndereco; 
+    private JButton btnSalvar, btnSair;
     public FormPessoa(){
         setSize (500,500);
         setTitle("Minha janelinha");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        var label = new JLabel ("Eu não acredito");
+        painelCampos();
+        painelBotoes();
 
-        getContentPane().add(label);
-        
         setVisible(true);
+        }
+    
+        private void painelBotoes(){
+            ControllerPessoa controller = new ControllerPessoa();
+
+
+
+            JPanel painel = new JPanel();
+            painel.setLayout(new FlowLayout(FlowLayout.CENTER));
+            btnSalvar = new JButton("Salvar");
+            painel.add(btnSalvar);
+            btnSalvar.setName("btnSalvar");
+            btnSalvar.addActionListener(controller);
+
+
+            btnSair = new JButton("Sair");
+            painel.add(btnSair);
+            btnSair.addActionListener(controller);
+            btnSair.setName("btnSair");
+            getContentPane().add(painel, "South");
+        }
+
+    private void painelCampos(){
+        JPanel painel = new JPanel();
+        painel.setLayout(new GridLayout(3,2));
+
+        lblID = new JLabel("ID");
+        painel.add(lblID);
+        txtId = new JTextField(5);
+        painel.add(txtId);
+
+        lblNome = new JLabel("Nome");
+        painel.add(lblNome);
+        txtNome = new JTextField(15);
+        painel.add(txtNome);
+
+        lblEndereco = new JLabel("Endereço");
+        painel.add(lblEndereco);
+        txtEndereco = new JTextField(15);
+        painel.add(txtEndereco);
+
+        getContentPane().add(painel, "North");
 
     }
     public static void main(String[] args){
