720304963aae8cb4693e62ac18a57ea33f7921aa
korrones <karolinaferreira518@gmail.com>
2025-03-26 20:16:54 -0300

    Swing - Forms e Botões

---
+++ revisao_karolina/src/main/java/br/univille/controller/ControllerPessoa.java
@@ -0,0 +1,16 @@
+package br.univille.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+public class ControllerPessoa implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null, "EU N ACREDITO"); 
+    }
+    
+
+}
--- revisao_karolina/src/main/java/br/univille/view/FormPessoa.java
+++ revisao_karolina/src/main/java/br/univille/view/FormPessoa.java
@@ -1,20 +1,79 @@
 package br.univille.view;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import br.univille.controller.ControllerPessoa;
+
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.lang.ModuleLayer.Controller;
+import java.math.BigInteger;
+
+/*Arquitetura MVC- Model, View e Controller
+ * 
+ * Model - Dados
+ * View - Front
+ * Controller - Controle de eventos
+ */
 
 public class FormPessoa extends JFrame {
+    private JButton btnSalvar, btnSair;
+    private JLabel lblId, lblNome, lblEndereco;
+    private JTextField txtId, txtNome, txtEndereco;
 
     public FormPessoa(){
         setSize(500, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Minha Janelinha");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        painelCampos();
+        painelBotoes();
 
-        var label = new JLabel("Eu não acredito");
-
-        getContentPane().add(label);
-        
         setVisible(true);
+    }
+
+    private void painelBotoes(){
+
+        ControllerPessoa controller = new ControllerPessoa();
+
+        JPanel painel = new JPanel();
+        painel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        btnSalvar = new JButton("Salvar");
+        painel.add(btnSalvar);
+        btnSalvar.addActionListener(controller);
+
+        btnSair = new JButton("Sair");
+        btnSair.addActionListener(controller);
+        painel.add(btnSair);
+        getContentPane().add(painel, "South");
+
+
+
+
+    }
+
+    private void painelCampos(){
+        JPanel painel = new JPanel();
+        painel.setLayout(new GridLayout(3,2));
+        lblId = new JLabel("ID");
+        painel.add(lblId);
+        txtId = new JTextField(5);
+        painel.add(txtId);
+
+        lblNome = new JLabel("Nome");
+        painel.add(lblNome);
+        txtNome = new JTextField(15);
+        painel.add(txtNome);
+
+        lblEndereco = new JLabel("Endereço");
+        painel.add(lblEndereco);
+        txtEndereco = new JTextField(15);
+        painel.add(txtEndereco);
+
+        getContentPane().add(painel, "North");
     }
 
     public static void main(String[] args) {
