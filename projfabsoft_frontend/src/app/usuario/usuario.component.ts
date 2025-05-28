40e89d0bd242c82ad9e63564a161082157f0c511
Karolina Ferreira <102107721+korrones@users.noreply.github.com>
2025-05-28 00:52:31 +0000

    usuario.component - table

--- projfabsoft_frontend/src/app/usuario/usuario.component.ts
+++ projfabsoft_frontend/src/app/usuario/usuario.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { Usuario } from '../model/usuario';
 import { UsuarioService } from '../service/usuario.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-usuario',
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './usuario.component.html',
-  styleUrl: './usuario.component.css'
+  styleUrl: './usuario.component.css',
+  providers: [UsuarioService]
 })
 export class UsuarioComponent {
 
