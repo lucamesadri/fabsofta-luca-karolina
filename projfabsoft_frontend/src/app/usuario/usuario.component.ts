30977247bbfa65df6794099eb1883239e9d6b54b
Karolina Ferreira <102107721+korrones@users.noreply.github.com>
2025-06-18 23:08:56 +0000

    alterações aula dia 17/08

--- projfabsoft_frontend/src/app/usuario/usuario.component.ts
+++ projfabsoft_frontend/src/app/usuario/usuario.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Usuario } from '../model/usuario';
 import { UsuarioService } from '../service/usuario.service';
-import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import * as bootstrap from 'bootstrap';
+
 @Component({
   selector: 'app-usuario',
   imports: [HttpClientModule, CommonModule],
@@ -14,24 +15,55 @@ import { Router } from '@angular/router';
 })
 export class UsuarioComponent {
 
-  public listaUsuarios:Usuario[] = [];
-  constructor(
-    private usuarioService:UsuarioService,
-    private router:Router
-  ){}
+  public listaUsuarios: Usuario[] = [];
 
-  ngOnInit(): void{
-    this.usuarioService.getUsuarios().subscribe(resposta =>{
+  @ViewChild('myModal') modalElement!: ElementRef;
+  private modal!: bootstrap.Modal;
+
+  private usuarioSelecionado!: Usuario;
+
+  constructor(
+    private usuarioService: UsuarioService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.usuarioService.getUsuarios().subscribe(resposta => {
       this.listaUsuarios = resposta;
-    })
+    });
   }
 
-  novo(){
+  novo() {
     this.router.navigate(['usuarios/novo']);
   }
 
-  alterar(usuario:Usuario){
+  alterar(usuario: Usuario) {
     this.router.navigate(['usuarios/alterar', usuario.id]);
   }
 
+  abrirConfirmacao(usuario: Usuario) {
+    this.usuarioSelecionado = usuario;
+    this.modal = new bootstrap.Modal(this.modalElement.nativeElement);
+    this.modal.show();
+  }
+
+  fecharConfirmacao() {
+    this.modal.hide();
+  }
+
+  confirmarExclusao() {
+    this.usuarioService.excluirUsuario(this.usuarioSelecionado.id).subscribe(
+      () => {
+        this.fecharConfirmacao();
+        this.usuarioService.getUsuarios().subscribe(
+          usuarios => {
+            this.listaUsuarios = usuarios;
+          }
+        );
+      },
+      error => {
+        console.error('Erro ao excluir usuário:', error);
+      }
+    );
+  }
 }
